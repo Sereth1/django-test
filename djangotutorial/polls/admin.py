072b9d2aa656cd9dd2a24e0d4e22f42cd9fb6caa
Sereth1 <papakias444@gmail.com>
2025-07-21 16:49:39 +0700

    a

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -1,20 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import (
-    Question, 
-    Book, 
-    Choise, 
-    Person,
-    Ram,
-    Cpu,
-    Gpu,
-    Motherboard,
-    Storage,
-    PowerSupply,
-    Case,
-    Computer
-)
+from .models import *
 
 
 models = [
